061cdccdf81c295c99ab80d6ca29b888acfbf7cb
BanLowi <lowi40k@gmail.com>
2026-04-18 14:12:47 +0200

    milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const products = [
   { name: 'Mela', price: 0.5 },
   { name: 'Pane', price: 1.2 },
@@ -6,6 +8,25 @@ const products = [
 ];
 
 function App() {
+
+  const [addedProducts, setAddedProducts] = useState([]);
+  console.log(addedProducts);
+
+  const addToCart = (prod) => {
+
+    const checkProd = addedProducts.find(p => p.name === prod.name)
+
+    if (!checkProd) {
+
+      setAddedProducts([...addedProducts, { name: prod.name, price: prod.price, quantity: 1 }])
+    } else {
+
+      setAddedProducts(addedProducts.map(p =>
+        p.name === prod.name ? { ...p, quantity: p.quantity + 1 } : p
+      ))
+    }
+
+  }
 
   return (
     <>
@@ -16,9 +37,12 @@ function App() {
       <main>
         <ul>
           {
-            products.map(p => <li><p>Nome: {p.name}, Prezzo: {p.price} €</p></li>)
+            products.map((p, index) => <li key={index}><p>Nome: {p.name}, Prezzo: {p.price} €</p> <button onClick={() => addToCart(p)}>Aggiungi al Carrello</button></li>)
           }
         </ul>
+        {
+          addedProducts && addedProducts.map((p, index) => <li key={index}><p>Nome: {p.name}, Prezzo: {(p.price * p.quantity).toFixed(2)} €</p></li>)
+        }
       </main>
 
     </>
